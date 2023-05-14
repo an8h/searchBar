2a5e4e65f3c66816aaab5f1c6ac8a14b72dec12f
Anthoula Alipasali <anthie10@hotmail.com>
2023-05-14 19:09:48 +0200

    add tests for SearchBar

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -1,37 +1,53 @@
 import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import SearchBar from "./SearchBar";
 
 describe("SearchBar", () => {
+  const onSearchMock = jest.fn();
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render without crashing", () => {
+    render(<SearchBar onSearch={onSearchMock} />);
+  });
+
   it("renders the search bar with a placeholder", () => {
     render(<SearchBar onSearch={() => {}} />);
     const inputElement = screen.getByPlaceholderText("Search");
     expect(inputElement).toBeInTheDocument();
   });
 
-  // it("calls the onSearch function when the form is submitted", () => {
-  //   const onSearch = jest.fn();
-  //   render(<SearchBar onSearch={onSearch} />);
-  //   const inputElement = screen.getByPlaceholderText("Search");
-  //   const searchTerm = "test";
-  //   fireEvent.change(inputElement, { target: { value: searchTerm } });
-  //   fireEvent.submit(screen.getByRole("search"));
-  //   expect(onSearch).toHaveBeenCalledWith(searchTerm);
-  // });
-  it("should call onSearch with the search term when the form is submitted", async () => {
-    // render(<SearchBar onSearch={() => {}} />);
-    const onSearch = jest.fn();
-    render(<SearchBar onSearch={onSearch} />);
-    const input = screen.getByPlaceholderText("Search");
+  it("should call onSearch prop when submit form", () => {
+    render(<SearchBar onSearch={onSearchMock} />);
     const form = screen.getByRole("search");
-    // const form = screen.getByRole("form");
-    const searchTerm = "test";
+    const input = screen.getByRole("textbox");
+    const searchTerm = "example";
 
     fireEvent.change(input, { target: { value: searchTerm } });
     fireEvent.submit(form);
 
-    await waitFor(() => {
-      expect(onSearch).toHaveBeenCalledWith(searchTerm);
-    });
+    expect(onSearchMock).toHaveBeenCalledWith(searchTerm);
+  });
+
+  it("should clear the input when clear icon is clicked", () => {
+    render(<SearchBar onSearch={onSearchMock} />);
+    const input = screen.getByLabelText("Search");
+    const searchTerm = "test";
+    fireEvent.change(input, { target: { value: searchTerm } });
+    expect(input).toHaveValue(searchTerm);
+
+    const clearIcon = screen.getByRole("button");
+
+    fireEvent.click(clearIcon);
+    expect(input).toHaveValue("");
+  });
+
+  it("should focus the input on mount", () => {
+    render(<SearchBar onSearch={onSearchMock} />);
+    const input = screen.getByLabelText("Search");
+
+    expect(input).toHaveFocus();
   });
 });
