ad3b13c259e3f27c3301c176d1e78e4c85e19569
Anthoula Alipasali <anthie10@hotmail.com>
2023-05-15 21:16:39 +0200

    add tests for SearchBar

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import SearchBar from "./SearchBar";
+import * as SearchService from "../../api/SearchService";
+import { SearchResult } from "../../types/types";
+
+jest.mock("../../api/SearchService");
 
 describe("SearchBar", () => {
   const onSearchMock = jest.fn();
@@ -22,7 +26,7 @@ describe("SearchBar", () => {
   it("should call onSearch prop when submit form", () => {
     render(<SearchBar onSearch={onSearchMock} />);
     const form = screen.getByRole("search");
-    const input = screen.getByRole("textbox");
+    const input = screen.getByRole("combobox");
     const searchTerm = "example";
 
     fireEvent.change(input, { target: { value: searchTerm } });
@@ -49,5 +53,43 @@ describe("SearchBar", () => {
     const input = screen.getByLabelText("Search");
 
     expect(input).toHaveFocus();
+  });
+
+  it("retrieves data when given a search query equal to or longer than 2 characters", async () => {
+    const searchResults: SearchResult[] = [
+      { id: 1, title: "result1", description: "Description 1", url: "url1" },
+      { id: 2, title: "result2", description: "Description 2", url: "url2" },
+    ];
+    jest.spyOn(SearchService, "default").mockResolvedValue(searchResults);
+
+    render(<SearchBar onSearch={onSearchMock} />);
+    const input = screen.getByLabelText("Search");
+
+    fireEvent.change(input, { target: { value: "query" } });
+    fireEvent.keyDown(input, { key: "Enter" });
+
+    await screen.findByText("result1");
+    expect(screen.getByText("result1")).toBeInTheDocument();
+    expect(screen.getByText("result2")).toBeInTheDocument();
+  });
+
+  it("displays search results in a list when data is retrieved successfully", async () => {
+    const mockResults: SearchResult[] = [
+      { id: 1, title: "result1", description: "Description 1", url: "url1" },
+      { id: 2, title: "result2", description: "Description 2", url: "url2" },
+      { id: 3, title: "result3", description: "Description 3", url: "url3" },
+    ];
+    jest.spyOn(SearchService, "default").mockResolvedValue(mockResults);
+
+    render(<SearchBar onSearch={onSearchMock} />);
+    const input = screen.getByLabelText("Search");
+
+    fireEvent.change(input, { target: { value: "valid input" } });
+    fireEvent.submit(screen.getByRole("search"));
+
+    await screen.findByText("result1");
+    expect(screen.getByText("result1")).toBeInTheDocument();
+    expect(screen.getByText("result2")).toBeInTheDocument();
+    expect(screen.getByText("result3")).toBeInTheDocument();
   });
 });
