80770c01f69bac2e652cf7b61e258aaee7b9c1b0
Anthoula Alipasali <anthie10@hotmail.com>
2023-05-15 21:49:53 +0200

    cleanup types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,12 +1,6 @@
-type SearchResult = {
+export type SearchResult = {
   id: number;
   title: string;
   description: string;
   url: string;
 };
-
-interface SearchResultWithHighlight extends SearchResult {
-  highlightedText: string;
-}
-
-export type { SearchResult, SearchResultWithHighlight };
